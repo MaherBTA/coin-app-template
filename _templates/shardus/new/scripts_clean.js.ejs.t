08a4ca2e4e6c08d7544239743745441a7fcd8da7
asyed94 <asyed94@gmail.com>
2019-07-12 13:11:31 -0500

    Make start script log monitor-client URL.

--- _templates/shardus/new/scripts_clean.js.ejs.t
+++ _templates/shardus/new/scripts_clean.js.ejs.t
@@ -6,7 +6,6 @@ const { rm } = require('shelljs')
 
 async function main () {
   try {
-    await execa('yarpm', 'run pm2 kill'.split(' '), { stdio: [0, 1, 2] })
     rm('-rf', './.pm2 ./db ./logs ./statistics.tsv'.split(' '))
   } catch (e) {
     console.log(e)
--- scripts/clean.js
+++ scripts/clean.js
@@ -3,7 +3,6 @@ const { rm } = require('shelljs')
 
 async function main () {
   try {
-    await execa('yarpm', 'run pm2 kill'.split(' '), { stdio: [0, 1, 2] })
     rm('-rf', './.pm2 ./db ./logs ./statistics.tsv'.split(' '))
   } catch (e) {
     console.log(e)
