15ced44dbee4a0a78c605f4159fd62690a129833
asyed94 <asyed94@gmail.com>
2019-11-18 15:27:34 -0600

    Updates npm clean script to include archive-server db

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,6 +3,7 @@ const { rm } = require('shelljs')
 async function main () {
   try {
     rm('-rf', './.pm2 ./db ./logs ./statistics.tsv'.split(' '))
+    rm('-rf', './archiver-db.sqlite'.split(' '))
   } catch (e) {
     console.log(e)
   }
