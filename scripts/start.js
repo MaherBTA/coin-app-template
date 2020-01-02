7f4987bd57b3cd843e4a0885d304cab328fa1255
asyed94 <asyed94@gmail.com>
2020-01-02 14:25:26 -0600

    Passes --no-autorestart option to PM2.

--- scripts/start.js
+++ scripts/start.js
@@ -2,8 +2,8 @@ const execa = require('execa')
 
 async function main () {
   try {
-    await execa('yarpm', 'run pm2 start ./node_modules/archive-server/bin/archive-server'.split(' '), { stdio: [0, 1, 2] })
-    await execa('yarpm', 'run pm2 start ./node_modules/monitor-server/bin/monitor-server'.split(' '), { stdio: [0, 1, 2] })
+    await execa('yarpm', 'run pm2 start --no-autorestart ./node_modules/archive-server/bin/archive-server'.split(' '), { stdio: [0, 1, 2] })
+    await execa('yarpm', 'run pm2 start --no-autorestart ./node_modules/monitor-server/bin/monitor-server'.split(' '), { stdio: [0, 1, 2] })
     console.log()
     console.log('\x1b[33m%s\x1b[0m', 'View network monitor at:') // Yellow
     console.log('  http://localhost:\x1b[32m%s\x1b[0m', '3000') // Green
